a9bfa9ec1ea356c4323b8ff616efc98849b3ff52
Jefferson Esteban <jeffersonestebanniquenmaqui@gmail.com>
2026-05-01 09:25:11 -0500

    Creación de la carpeta Formularios/ y agregado de los UserControl de Factura y Pedidos (Vacíos y Manipulables). Tamaño estimado recomendado para los formularios creados más adelante [1050, 605] en Size, propiedades de los Forms o UserControl

--- GUI_V_2/DashboardMetricMenu.Designer.cs
+++ GUI_V_2/DashboardMetricMenu.Designer.cs
@@ -585,6 +585,7 @@
             // 
             // InicioResumen
             // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Clock;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(195)))), ((int)(((byte)(195)))));
--- GUI_V_2/SideBarMenu.Designer.cs
+++ GUI_V_2/SideBarMenu.Designer.cs
@@ -399,6 +399,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SideBarMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.SideBarMenu_Load);
             this.MenuVertical.ResumeLayout(false);
